26685d29365f2ff3f9b30158c441a11844f17d1d
Vicente P. Santos <vicentedpsantos@gmail.com>
2018-04-04 02:08:43 -0300

    added options to context menu such as calling, texting and google maps

--- app/src/main/java/br/com/vicente/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/vicente/agenda/ListaAlunosActivity.java
@@ -1,6 +1,12 @@
 package br.com.vicente.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Browser;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -69,13 +75,62 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Contato contato = (Contato) listaContatos.getItemAtPosition(info.position);
+
+        //Criação da opção de ligar para contato através do menu de contexto
+        MenuItem ligar = menu.add("Call");
+        ligar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)!=PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                                                        new String[]{Manifest.permission.CALL_PHONE}, 01);//o request code serve para diferenciar os requests por permissão.
+                                                                                                                      // atraves do metodo onRequestPermissionResult, é possível mudar o comportamento do código
+                                                                                                                      // para cada request code diferente que houver.
+                } else {
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + contato.getTelefone()));
+
+                    startActivity(intentLigar);
+                    return false;
+                }
+
+                return false;
+            }
+        });
+
+        //Criação da opção de visualizar no mapa
+        String enderecoDoContato = contato.getEndereco();
+        MenuItem verNoMapa = menu.add("Location on map");
+        Intent intentVerNoMapa = new Intent(Intent.ACTION_VIEW);
+        verNoMapa.setIntent(intentVerNoMapa);
+        intentVerNoMapa.setData(Uri.parse("geo:0,0?q="+enderecoDoContato));
+
+        //Criação da opção de enviar SMS ao contato
+        String numeroTelefone = contato.getTelefone();
+        MenuItem enviarMensagem = menu.add("Send text");
+        Intent intentEnviarMensagem = new Intent(Intent.ACTION_VIEW);
+        intentEnviarMensagem.setData(Uri.parse("sms:" + numeroTelefone));
+        enviarMensagem.setIntent(intentEnviarMensagem);
+
+        //Criação da opção de ir até o website cadastrado no contato
+        MenuItem irParaWebsite = menu.add("Go to Website");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+
+        String site = contato.getSite();
+
+        if (!site.startsWith("http://")){
+            site = "http://" + site;
+        }
+        intentSite.setData(Uri.parse(site));
+        irParaWebsite.setIntent(intentSite);
+
+        //Criação da opção de deletar contato no menu de contexto
         MenuItem delete = menu.add("Delete");
         delete.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Contato contato = (Contato) listaContatos.getItemAtPosition(info.position);
-
                 ContatoDAO dao = new ContatoDAO(ListaAlunosActivity.this);
                 dao.deleta(contato);
                 dao.close();
@@ -85,5 +140,4 @@ public class ListaAlunosActivity extends AppCompatActivity {
             }
         });
     }
-
 }
